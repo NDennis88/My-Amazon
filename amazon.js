b881587e288bf64c36985a80657cfb358f254505
Nai-Muh Dennis <naimuhdennis@gmail.com>
2019-03-14 18:31:12 -0700

    Stock Check

--- amazon.js
+++ amazon.js
@@ -25,26 +25,37 @@ connection.connect(function(err) {
         message: "What is the item id for the product you would like to search for?"
       })
       .then(function(answer) {
-          console.log(answer)
+        //   console.log(answer)
         var query = "SELECT item_id, product_name, price, stock_quantity FROM products WHERE ?";
         connection.query(query, { item_id: answer.item_id }, function(err, res) {
           console.table(res);
-          askQuantity();
-          runSearch();
+          askQuantity(answer.item_id);
+        //   runSearch();
         });
       });
     });
   }
 
-  function askQuantity(){
-      inquirer
-        .prompt({
-            name: "product_quantity",
-            type: "input",
-            message: "How many units of product would you like to order?"
-        })
-        .then(function(answer){
-            console.log(answer)
-        // var query = 
-        })
-  }
+  function askQuantity(item_id){
+    connection.query("SELECT stock_quantity FROM products WHERE ?", { item_id: parseInt(item_id) }, function(err, res) {
+        console.log(res);
+        var currentStock = res[0].stock_quantity;
+        console.log(currentStock);
+        connection.end();
+    //     if (err) throw err;
+    //   inquirer
+    //     .prompt({
+    //         name: "product_quantity",
+    //         type: "input",
+    //         message: "How many units of product would you like to order?"
+    //     })
+    //     .then(function(answer){
+    //     var chosenItem;
+    //     if(chosenItem.stock_quantity < parseInt(answer.product_quantity)) {
+    //         connection.query("UPDATE products SET ? WHERE ?",
+    //         stock_quantity: stock_quantity - answer.product_quantity,
+    //         )
+    //     }
+    //     })
+  })
+}
