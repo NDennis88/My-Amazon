76b7e4aef24d1257828009c4293b1f90ff633fe3
Nai-Muh Dennis <naimuhdennis@gmail.com>
2019-03-15 17:37:18 -0700

    working price/inventory update

--- amazon.js
+++ amazon.js
@@ -15,7 +15,7 @@ connection.connect(function(err) {
   });
   
   function itemSearch() {
-    connection.query("SELECT * FROM products", function(err,res){
+    connection.query("SELECT item_id, product_name, price FROM products", function(err,res){
         if(err) throw err;
         console.table(res);
     inquirer
@@ -26,36 +26,47 @@ connection.connect(function(err) {
       })
       .then(function(answer) {
         //   console.log(answer)
-        var query = "SELECT item_id, product_name, price, stock_quantity FROM products WHERE ?";
+        var query = "SELECT item_id, product_name, price FROM products WHERE ?";
         connection.query(query, { item_id: answer.item_id }, function(err, res) {
           console.table(res);
           askQuantity(answer.item_id);
         //   runSearch();
         });
-      });
+      })
     });
   }
 
   function askQuantity(item_id){
-    connection.query("SELECT stock_quantity FROM products WHERE ?", { item_id: parseInt(item_id) }, function(err, res) {
-        console.log(res);
-        var currentStock = res[0].stock_quantity;
-        console.log(currentStock);
-        connection.end();
-    //     if (err) throw err;
-    //   inquirer
-    //     .prompt({
-    //         name: "product_quantity",
-    //         type: "input",
-    //         message: "How many units of product would you like to order?"
-    //     })
-    //     .then(function(answer){
-    //     var chosenItem;
-    //     if(chosenItem.stock_quantity < parseInt(answer.product_quantity)) {
-    //         connection.query("UPDATE products SET ? WHERE ?",
-    //         stock_quantity: stock_quantity - answer.product_quantity,
-    //         )
-    //     }
-    //     })
+    inquirer.prompt({
+        name: "product_quantity",
+        type: "input",
+        message: "How many units of product would you like to order?"
+    }).then(function (user) {
+      connection.query("SELECT stock_quantity, price FROM products WHERE ?", { item_id: parseInt(item_id) }, function (err, res) {
+         if (err) throw err;
+         for (var i = 0; i < res.length; i++) {
+
+         }
+           var currentStock = res[0].stock_quantity;
+           var price = res[0].price;
+          //  console.log(currentStock);
+           if (user.product_quantity > currentStock) {
+             console.log("Apologies! We're currently out of stock.");
+             // askQuantity();
+             itemSearch();
+           } else {
+             console.log("We got your back like chiropractic! Your items have been successfully ordered");
+            //  console.log("Quantity: " + currentStock - user.quantity);
+             // console.log("----------------");
+             updatedQuanity= parseInt(currentStock) - parseInt(user.product_quantity)
+            //  console.log("below should be 47")
+             console.log(updatedQuanity);
+             connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?", [updatedQuanity, item_id ], function (err, res) {
+                 console.log("updated");
+              console.log("Your total is: $" + user.product_quantity * price);
+              connection.end();
+             })
+        }
+    });
   })
 }
